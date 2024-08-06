24a10146b5ac09eecfcfb435cc0deaa571382584
Josh <josh2507@hotmail.co.uk>
2024-08-06 14:17:50 +0100

    struggling

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,12 +1,11 @@
-export function calculateScrabbleScore(word) {}
-//Write software using TDD Approach (Red -> Green -> refactor)
+export function calculateScrabbleScore(word) {
+// //Write software using TDD Approach (Red -> Green -> refactor)
 
-//Declare a variable that detects user input
-const userInput = prompt("Enter any letter from A-Z") 
+// //Declare a variable that detects user input
+const userInput = ("Enter any letter from A-Z");
         if (userInput === "A")
     return 1;
-
+}
 //Organise with Numerical order (Ascending or Decending)
-
 
 //Score should be displayed correctly in ASC or DESC
--- scrabble-score.test.js
+++ scrabble-score.test.js
@@ -1,1 +1,6 @@
 import { calculateScrabbleScore } from "./scrabble-score";
+import { test, expect } from 'vitest';
+
+test("The Score is", () => {
+    expect(calculateScrabbleScore("A")).toBe(1);
+})
