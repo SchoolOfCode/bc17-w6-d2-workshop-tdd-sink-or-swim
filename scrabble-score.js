ab8ffce23a2264ecc8c5171202bb61c442e3ba5c
Josh <josh2507@hotmail.co.uk>
2024-08-06 14:27:26 +0100

    fixed scrabble score

--- scrabble-score.js
+++ scrabble-score.js
@@ -2,8 +2,7 @@ export function calculateScrabbleScore(word) {
 // //Write software using TDD Approach (Red -> Green -> refactor)
 
 // //Declare a variable that detects user input
-const userInput = ("Enter any letter from A-Z");
-        if (userInput === "A")
+        if (word === "A")
     return 1;
 }
 //Organise with Numerical order (Ascending or Decending)
